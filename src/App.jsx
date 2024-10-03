7a3dc08a1403e85ac37ffa80c4a1d9dcb776d169
ashishbehera29 <87660082+ashishbehera29@users.noreply.github.com>
2024-10-03 09:07:04 +0530

    REACTJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import Component1 from "./Component1";
+import Car from "./Car";
+import Cash from "./Cash";
+import Hp from "./Hp";
 
 function App(){
     console.log("welcome to react js");
 
     return(
         <>
-           <h1>hello ashish</h1>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nisi nihil beatae fugit id soluta mollitia, itaque earum odio qui est aperiam sapiente ullam magnam accusamus corporis eligendi voluptatum odit.</p>
-           <Component1/>
-           <Component1/>
-           <Component1/>
-
+         <Cash/>
         </>
     )
     
---
+++ src/Lenovo.jsx
@@ -0,0 +1,16 @@
+
+const Lenovo = (props) => {
+    console.log(props);
+    let b=props.data;
+    console.log(b);
+    
+    
+  return (
+    <div>
+      <h1>i am leneovo component</h1>
+      
+    </div>
+  )
+}
+
+export default Lenovo
