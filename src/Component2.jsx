a0247d1e143bcd53520d7b59d25f80d0e1251e20
ashishbehera29 <ashishbehera8382@gmail.com>
2025-02-20 22:16:26 +0530

    bgcolor

--- src/Component2.jsx
+++
@@ -1,6 +0,0 @@
-  function Component2(){
-      return(
-        <h1>hello</h1>
-      )
-  }
-  export default Component2
---
+++ src/assignment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const assignment = () => {
+  return (
+    <div>
+        <button>ashish</button>
+    </div>
+  )
+}
+
+export default assignment
